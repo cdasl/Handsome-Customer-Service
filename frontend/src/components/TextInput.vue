c0a3d1b2273bbc925644d4524f186d4f0c11cb15
hehe <762373701@qq.com>
2017-08-01 18:32:39 +0800

    Create inner user dialog.  Ref #3

---
+++ frontend/src/components/TextInput.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="text">
+  <textarea placeholder="按 Ctrl + Enter 发送"></textarea>
+</div>
+</template>
+<script>
+</script>
+<style lang="less" scoped>
+.text {
+    height: 160px;
+    border-top: solid 1px #ddd;
+
+    textarea {
+        padding: 10px;
+        height: 100%;
+        width: 100%;
+        border: none;
+        outline: none;
+        font-family: "Micrsofot Yahei";
+        resize: none;
+    }
+}
+</style>
