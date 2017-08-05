e7de3ab65d7aa72ffc802dd961ea7b97a4d8e4fd
黄德军 <1968486175@qq.com>
2017-08-05 21:19:22 +0800

    Add setting user message.  Ref #27

--- backend/urls.py
+++ backend/urls.py
@@ -39,4 +39,5 @@ urlpatterns = [
     url(r'^api/reset_password/$', enterprise.reset_password_request, name = 'reset_pwd_request'),
     url(r'^api/new_pwd_submit/$', enterprise.reset_password, name = 'reset_password_submit'),
     url(r'^api/enter/set_robot_message/$', enterprise.enterprise_set_robot_message, name = 'enterprise_set_robot_message'),
+    url(r'^api/enter/setuser_message/$', enterprise.enterprise_setuser_message, name = 'enterprise_setuser_message'),
 ]
