6a8f0b552e285cd635f19067d385ad0dc119dcd2
黄德军 <1968486175@qq.com>
2017-08-14 20:16:10 +0800

    Add two files.  Ref #173

---
+++ backend/const_table.py
@@ -0,0 +1,23 @@
+from . import const
+
+const.SUCCESS = 'success'#1
+const.WRONG_PASSWORD = 'wrong password'#-1
+const.FAIL_MODIFY = 'fail to midify'#-2
+const.EMAIL_REGISTERED = 'this email has been registered'#-3
+const.FAIL_SIGN_UP = 'fail to sign up'#-4
+const.ACCOUNT_NOT_ACTIVETED = 'account not activeted'#-5
+const.ACCOUNT_LOGGED_OFF = 'account has been logged off'#-6
+const.WRONG_ACCOUNT = 'wrong account'#-7
+const.INVALID = 'invalid'#-8
+const.EXPIRED = 'expired'#-9
+const.MAILBOX_REGISTERED = 'the mailbox has been registered'#-10
+const.INVITE_FAILURE = 'invite failure'#-11
+const.ERROR = 'error'#-12
+const.CUSTOMER_NOT_EXIST = 'not exist this customer'#-13
+const.FAIL_LOG_OFF = 'fail to log off'#-14
+const.FAIL_INQUIRE_INFORMATION = 'fail to inquire information'#-15
+const.DIALOGID_NOT_EXIST = 'not exist this dialogID'#-16
+const.SID_NOT_EXIST = 'not exist this SID'#-17
+const.RID_NOT_EXIST = 'not exist this RID'#-18
+const.EID_NOT_EXIST = 'eid not in request.session'#-19
+const.CID_NOT_EXIST = 'cid not in request.session'#-20
